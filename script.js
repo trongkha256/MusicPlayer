a0f8c3108307aaaa26239b0fdc46bb137b8859e2
trongkha256 <trongkha256@gmail.com>
2022-04-18 20:25:58 +0700

    New Update

--- script.js
+++ script.js
@@ -26,47 +26,59 @@ const app = {
     // (1/2) Uncomment the line below to use localStorage
     // config: JSON.parse(localStorage.getItem(PlAYER_STORAGE_KEY)) || {},
     songs: [{
-            name: "Click Pow Get Down",
-            singer: "Raftaar x Fortnite",
-            path: "./music/Co-Dieu-Gi-Sao-Khong-Noi-Cung-Anh-Live-In-HOA-Concert-Trung-Quan-Idol.mp3",
-            image: "https://i.ytimg.com/vi/jTLhQf5KJSc/maxresdefault.jpg"
+            name: "DARARI",
+            singer: "TREASURE",
+            path: "./music/Darari-TREASUREBoyband-7130169.mp3",
+            image: "./img/maxresdefault.jpg"
         },
         {
-            name: "Tu Phir Se Aana",
-            singer: "Raftaar x Salim Merchant x Karma",
-            path: "https://mp3.vlcmusic.com/download.php?track_id=34213&format=320",
-            image: "https://1.bp.blogspot.com/-kX21dGUuTdM/X85ij1SBeEI/AAAAAAAAKK4/feboCtDKkls19cZw3glZWRdJ6J8alCm-gCNcBGAsYHQ/s16000/Tu%2BAana%2BPhir%2BSe%2BRap%2BSong%2BLyrics%2BBy%2BRaftaar.jpg"
+            name: "Ánh Nắng Của Anh",
+            singer: "Đức Phúc",
+            path: "./music/Anh-Nang-Cua-Anh-Cho-Em-Den-Ngay-Mai-OST-Duc-Phuc.mp3",
+            image: "./img/Tab Harmonica - Ánh Nắng Của Anh (Chờ Em Đến Ngày Mai OST) - Duc Phuc.png"
         },
         {
-            name: "Naachne Ka Shaunq",
-            singer: "Raftaar x Brobha V",
-            path: "https://mp3.filmysongs.in/download.php?id=Naachne Ka Shaunq Raftaar Ft Brodha V Mp3 Hindi Song Filmysongs.co.mp3",
-            image: "https://i.ytimg.com/vi/QvswgfLDuPg/maxresdefault.jpg"
+            name: "Muộn Rồi Mà Sao Còn",
+            singer: "Sơn Tùng- MTP",
+            path: "./music/MuonRoiMaSaoCon-SonTungMTP-7011803.mp3",
+            image: "./img/ab67616d0000b27329f906fe7a60df7777b02ee1.jpg"
         },
         {
-            name: "Mantoiyat",
-            singer: "Raftaar x Nawazuddin Siddiqui",
-            path: "https://mp3.vlcmusic.com/download.php?track_id=14448&format=320",
-            image: "https://a10.gaanacdn.com/images/song/39/24225939/crop_480x480_1536749130.jpg"
+            name: "Nàng Thơ",
+            singer: "Hoàng Dũng",
+            path: "./music/NangTho-HoangDung-6413381.mp3",
+            image: "./img/nangtho.jpg"
         },
         {
-            name: "Aage Chal",
-            singer: "Raftaar",
-            path: "https://mp3.vlcmusic.com/download.php?track_id=25791&format=320",
-            image: "https://a10.gaanacdn.com/images/albums/72/3019572/crop_480x480_3019572.jpg"
+            name: "Gone",
+            singer: "Rosé",
+            path: "./music/Gone-ROSE-6964052.mp3",
+            image: "./img/gone.jpg"
         },
         {
-            name: "Damn",
-            singer: "Raftaar x kr$na",
-            path: "https://mp3.filmisongs.com/go.php?id=Damn%20Song%20Raftaar%20Ft%20KrSNa.mp3",
-            image: "https://a10.gaanacdn.com/gn_img/albums/YoEWlabzXB/oEWlj5gYKz/size_xxl_1586752323.webp"
+            name: "Có Hẹn Với Thanh Xuân",
+            singer: "Monstar",
+            path: "./music/cohenvoithanhxuan-MONSTAR-7050201.mp3",
+            image: "./img/thanhxuan.jpg"
                 // image: "https://filmisongs.xyz/wp-content/uploads/2020/07/Damn-Song-Raftaar-KrNa.jpg"
         },
         {
-            name: "Feeling You",
-            singer: "Raftaar x Harjas",
-            path: "https://mp3.vlcmusic.com/download.php?track_id=27145&format=320",
-            image: "https://a10.gaanacdn.com/gn_img/albums/YoEWlabzXB/oEWlj5gYKz/size_xxl_1586752323.webp"
+            name: "MONEY   ",
+            singer: "LISA",
+            path: "./music/Money-LISA-7086698.mp3",
+            image: "./img/money.webp"
+        },
+        {
+            name: "Có Điều Gì Sao Không Nói Cùng Anh",
+            singer: "Trung Quân Idol",
+            path: "./music/Co-Dieu-Gi-Sao-Khong-Noi-Cung-Anh-Live-In-HOA-Concert-Trung-Quan-Idol.mp3",
+            image: "./img/65fa3b325f95866bc5251c041f223643.jpg"
+        },
+        {
+            name: "Sài Gòn Đau Lòng Quá",
+            singer: "Hoàng Duyên",
+            path: "./music/SaiGonDauLongQua-HuaKimTuyenHoangDuyen-6992977.mp3",
+            image: "./img/SG.jpeg"
         }
     ],
     render: function() {
